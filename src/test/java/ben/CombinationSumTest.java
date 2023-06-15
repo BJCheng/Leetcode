ec360eaf6854399d6d745123ee507cd0f7e2de70
Ben <benjencheng62@gmail.com>
2023-06-15 21:12:01 +0800

    CombinationSum

---
+++ src/test/java/ben/CombinationSumTest.java
@@ -0,0 +1,36 @@
+package ben;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class CombinationSumTest {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<Integer> path = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        dfs(0, result, path, candidates, target);
+        return result;
+    }
+
+    public void dfs(int sum, List<List<Integer>> result, List<Integer> path, int[] candidates, int target) {
+        if (sum > target) {
+            return;
+        }
+        if (sum == target) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < candidates.length; i++) {
+            path.add(candidates[i]);
+            dfs(sum + target, result, path, candidates, target);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    @Test
+    void test() {
+        new CombinationSumTest().combinationSum(new int[] { 2, 3, 5 }, 8);
+    }
+}
